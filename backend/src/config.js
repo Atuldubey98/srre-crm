9b09a94d9d94b94890f7d0df71d6e3b017546511
Atuldubey98 <atuldubey017@gmail.com>
2023-08-18 17:19:23 +0530

    fix : env feature

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,7 +1,8 @@
 import * as dotenv from "dotenv";
-export const NODE_ENV = process.env.NODE_ENV || "development";
+export const NODE_ENV = process.env.NODE_ENV;
+const path = NODE_ENV === "development" ? "../.env.development" : "../.env";
 dotenv.config({
-  path: NODE_ENV === "development" ? "../.env.development" : "../../.env",
+  path,
 });
 export const SECRET_ADMIN_KEY = process.env.SECRET_ADMIN_KEY;
 export const MONGO_URI = process.env.MONGO_URI;
@@ -10,3 +11,4 @@ export const PORT = isNaN(Number(process.env.VITE_API_PORT))
   : Number(process.env.VITE_API_PORT);
 export const JWT_EXPIRE = process.env.JWT_EXPIRE || "1d";
 export const JWT_SECRET = process.env.JWT_SECRET || "";
+ 
