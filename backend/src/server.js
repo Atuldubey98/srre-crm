8b19d83c70ae6eb2411b302aeb0db7d1cbec4457
Atuldubey98 <atuldubey017@gmail.com>
2023-09-04 16:32:54 +0530

    feature : cluster

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,31 @@
 import app from "./app.js";
 import http from "http";
 import { PORT } from "./config.js";
+import mongoose from "mongoose";
 
-const server = http.createServer(app);
-
-server.listen(PORT, () => {
-  console.log("Server is running");
-});
+import cluster from "cluster";
+import { availableParallelism } from "os";
+const noOfCpus = availableParallelism();
+if (cluster.isPrimary) {
+  for (let i = 0; i < noOfCpus; i++) {
+    cluster.fork();
+  }
+  cluster.on("exit", (worker) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  const server = http.createServer(app);
+  server.listen(PORT, () => {
+    console.log("Server is running");
+  });
+  process.on("SIGTERM", () => {
+    console.info("SIGTERM signal received.");
+    console.log("Closing http server.");
+    server.close(() => {
+      console.log("Http server closed.");
+      mongoose.connection.close(false, () => {
+        console.log("MongoDb connection closed.");
+      });
+    });
+  });
+}
